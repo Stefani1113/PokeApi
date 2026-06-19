949e69f14b5f5da4bc712c8dc0937b1842ffe067
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-19 06:25:10 -0500

    feat: Creación de models

---
+++ src/app/features/pokemones/models/poke-req.model.ts
@@ -0,0 +1,15 @@
+export interface PokeReq{
+    name: string;
+    abilities:{
+        ability: {
+            name : string;
+        }
+        is_hidden: boolean,
+        slot: number;
+    }
+    base_experience: number;
+    sprites:{
+        back_default: string;
+        front_default: string;
+    }
+}
